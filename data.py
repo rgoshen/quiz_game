d5afb816f980102917a8051d78b717fabc2746f6
Richard Goshen <rick.goshen@gmail.com>
2020-12-03 13:54:38 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,117 @@
+# question_data = [
+#     {"question": "A slug's blood is green.", "correct_answer": "True"},
+#     {"question": "The loudest animal is the African Elephant.", "correct_answer": "False"},
+#     {"question": "Approximately one quarter of human bones are in the feet.", "correct_answer": "True"},
+#     {"question": "The total surface area of a human lungs is the size of a football pitch.", "correct_answer": "True"},
+#     {"question": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "correct_answer": "True"},
+#     {"question": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "correct_answer": "False"},
+#     {"question": "It is illegal to pee in the Ocean in Portugal.", "correct_answer": "True"},
+#     {"question": "You can lead a cow down stairs but not up stairs.", "correct_answer": "False"},
+#     {"question": "Google was originally called 'Backrub'.", "correct_answer": "True"},
+#     {"question": "Buzz Aldrin's mother's maiden name was 'Moon'.", "correct_answer": "True"},
+#     {"question": "No piece of square dry paper can be folded in half more than 7 times.", "correct_answer": "False"},
+#     {"question": "A few ounces of chocolate can to kill a small dog.", "correct_answer": "True"}
+# ]
+
+question_data = [
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Gumbo is a stew that originated in Louisiana.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Nutella is produced by the German company Ferrero.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Bulls are attracted to the color red.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The Sun rises from the North.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The color orange is named after the fruit.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "In 2010, Twitter and the United States Library of Congress partnered together to archive every tweet by American citizens.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian. ",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "A pasodoble is a type of Italian pasta sauce.",
+            "correct_answer": "False",
+            "incorrect_answers": [
+                "True"
+            ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Slovakia is a member of European Union-",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False"
+            ]
+        }
+    ]
